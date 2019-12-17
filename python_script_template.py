4c4a9898de557bc4cb8cba24b0740f36821056ef
Erin Gilbert <gilbe952@umn.edu>
2019-12-17 03:02:03 -0600

    Create python_script_template.py

---
+++ python_script_template.py
@@ -0,0 +1,49 @@
+# Project: SCRIPT_NAME.py
+# Author: Erin Gilbert
+# Created: DATE
+# Updated: DATE
+
+#Usage:  USAGE HERE
+#Purpose: DECRIPTION HERE
+
+
+############################################
+#####    Define Environment/Packages  ######
+############################################
+
+
+#!/usr/bin/python
+
+##commonly used packages for me
+import sys, traceback
+import cv2
+import numpy as np
+import argparse
+import string
+sys.path.append("/Users/gilbe952/Documents/plantcv") #remember to set the location of packages if necessary
+import plantcv
+from plantcv import plantcv as pcv
+import PIL
+
+############################################
+######    Define Functions     #############
+############################################
+
+
+
+############################################
+######    Load All the Data    #############
+############################################
+
+
+
+
+############################################
+######    Perform Calculations    ##########
+############################################
+
+
+
+############################################
+###########    Create Output   #############
+############################################
